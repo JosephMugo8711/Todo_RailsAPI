46f77ccb2d610529032f190a56265e0ca501a7cc
josephwamiti <wamiti8711@gmail.com>
2023-05-27 20:33:58 +0300

    add private methods

--- app/controllers/todos_controller.rb
+++ app/controllers/todos_controller.rb
@@ -1,2 +1,23 @@
 class TodosController < ApplicationController
+
+
+    
+    private
+
+    def todo_params
+        params.permit(:name, :description, :completed)
+    end
+
+    def find_todo
+        todo.find(params[:id])
+    end
+
+    def todo_not_found
+        render json: {error: 'todo not found'}, status: :not_found
+    end
+
+    def unprocessable_entity invalid
+        render json: {error: invalid.record.errors.full_messages}, status: :unprocessable_entity
+    end
+    
 end
